08785ed2fd145c69f099e16d480837aeee468de6
Yuan Li <yli@pacificbiosciences.com>
2017-07-28 15:56:04 -0700

    Add substr_fasta, a debugging tool

---
+++ pbsv_polish/substr_fasta.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from argparse import ArgumentParser
+from collections import defaultdict
+import os
+import os.path as op
+import sys
+import json
+
+from pbsv.independent.utils import is_fasta
+from pbsv.libs import Fastafile, AlignmentFile
+
+def get_parser():
+    """Set up and return argument parser."""
+    parser = ArgumentParser("Extract a substring (chrom, start, end) from input FASTA file and save to output FASTA file.")
+    parser.add_argument("input_fq_fn", help="Input FASTA filename")
+    parser.add_argument("chrom", help="Name of the sequence to extract")
+    parser.add_argument("start", help="Start position of substring, 0-based, inclusive")
+    parser.add_argument("end", help="End position of substring, 0-based, exclusive")
+    parser.add_argument("output_fa_fn", help="Output FASTA filename")
+    return parser
+
+from .utils import substr_fasta
+
+def run(args):
+    assert is_fasta(args.input_fa_fn) and is_fasta(args.output_fa_fn)
+    fileobj = Fastafile(args.input_fa_fn)
+    substr_fasta(fileobj=fileobj, chrom=args.chrom, start=args.start, end=args.end, o_fasta_fn=args.output_fa_fn)
+
+def main():
+    """main"""
+    sys.exit(run(get_parser().parse_args(sys.argv[1:])))
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -44,7 +44,8 @@ setup(
     entry_points={'console_scripts': [
         'sv_pbdagcon = pbsv_polish.sv_pbdagcon:main',
         'polish_sv = pbsv_polish.polish_sv:main',
-        'collect_polished_sv = pbsv_polish.collect_polished_sv:main'
+        'collect_polished_sv = pbsv_polish.collect_polished_sv:main',
+        'substr_fasta = pbsv_polish.substr_fasta:main'
     ]},
     install_requires=_get_requirements(_get_local_file(_REQUIREMENTS_FILE)),
     tests_require=['pytest'],

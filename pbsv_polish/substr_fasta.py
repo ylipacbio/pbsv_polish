eb85cff3e8905b237dc4332ebb3209bdafad731f
Yuan Li <yli@pacificbiosciences.com>
2017-07-28 16:20:06 -0700

    fix substr_fasta, format start and end to int

--- pbsv_polish/substr_fasta.py
+++ pbsv_polish/substr_fasta.py
@@ -13,7 +13,7 @@ from pbsv.libs import Fastafile, AlignmentFile
 def get_parser():
     """Set up and return argument parser."""
     parser = ArgumentParser("Extract a substring (chrom, start, end) from input FASTA file and save to output FASTA file.")
-    parser.add_argument("input_fq_fn", help="Input FASTA filename")
+    parser.add_argument("input_fa_fn", help="Input FASTA filename")
     parser.add_argument("chrom", help="Name of the sequence to extract")
     parser.add_argument("start", help="Start position of substring, 0-based, inclusive")
     parser.add_argument("end", help="End position of substring, 0-based, exclusive")
@@ -25,7 +25,7 @@ from .utils import substr_fasta
 def run(args):
     assert is_fasta(args.input_fa_fn) and is_fasta(args.output_fa_fn)
     fileobj = Fastafile(args.input_fa_fn)
-    substr_fasta(fileobj=fileobj, chrom=args.chrom, start=args.start, end=args.end, o_fasta_fn=args.output_fa_fn)
+    substr_fasta(fileobj=fileobj, chrom=str(args.chrom), start=int(args.start), end=int(args.end), o_fasta_fn=args.output_fa_fn)
 
 def main():
     """main"""

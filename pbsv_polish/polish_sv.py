d0db789125be18972759944f8909d4ef930cce8b
Yuan Li <yli@pacificbiosciences.com>
2017-07-31 15:36:07 -0700

    update polish_sv.py, remove dump code

--- pbsv_polish/polish_sv.py
+++ pbsv_polish/polish_sv.py
@@ -31,7 +31,7 @@ blasr polished.hqlq.fasta sv_reference_w_extension.fasta --header --maxMatch 15 
     with open(diagnose_fn, 'w') as w:
         w.write(cmd)
 
-def polish_a_sv(bed_record, alns, out_dir, subreads_ds_obj, reference_fasta_obj, make_reference_fa, make_subreads_bam, make_scripts, execute_scripts, min_qv, use_sge):
+def polish_a_sv(bed_record, alns, work_dir, subreads_ds_obj, reference_fasta_obj, make_reference_fa, make_subreads_bam, make_scripts, execute_scripts, min_qv, use_sge):
     """
     Given a structural variant (as bed_record) and its supportive alignments, polish the given structural variant.
     * if make_reference_fa is True, generate a substr of chromosome as reference for this structural variant
@@ -40,16 +40,13 @@ def polish_a_sv(bed_record, alns, out_dir, subreads_ds_obj, reference_fasta_obj,
     """
     srs = get_query_subreads_from_alns(alns)
     zmws = get_query_zmws_from_alns(alns)
-
-    sv_prefix = bed2prefix(bed_record)
-    data_dir = realpath(op.join(out_dir, sv_prefix))
-    svp_files_obj = SVPolishFiles(root_dir=data_dir, min_qv=min_qv)
+    svp_files_obj = SVPolishFiles(root_dir=work_dir, min_qv=min_qv)
     _mkdir(svp_files_obj.root_dir) # make a subdirectory (e.g., chrI_0_100_Deletion_-100) for all polishing files
 
     if make_reference_fa:
         # make a substring spanning the expected structural variants
         #ref_start, ref_end = max(0, bed_record.start - Constant.REFERENCE_EXTENSION), bed_record.end + Constant.REFERENCE_EXTENSION
-        ref_start, ref_end = get_ref_extension_for_sv(bed_record)
+        ref_start, ref_end = get_ref_extension_for_sv(bed_record, reference_fasta_obj.lengths[reference_fasta_obj.references(bed_record.chrom)])
         substr_fasta(fileobj=reference_fasta_obj, chrom=bed_record.chrom, start=ref_start, end=ref_end, o_fasta_fn=svp_files_obj.sv_ref_fa)
 
     if make_subreads_bam:
@@ -80,11 +77,13 @@ def run(args):
     bedreader_obj = BedReader(bed_fn)
 
     for bed_record, alns in yield_alns_from_bed_file(alnfile_obj, bedreader_obj=bedreader_obj):
-        log.info("Processing sv %s" % bed2prefix(bed_record))
-        print("Processing sv %s" % bed2prefix(bed_record))
         ofile_obj.write('%s\t%s\n' % (len(get_query_subreads_from_alns(alns)), bed_record)) # write coverage info
 
-        polish_a_sv(bed_record, alns, out_dir, subreads_ds_obj, reference_fasta_obj, make_reference_fa=True, make_subreads_bam=True, make_scripts=True, execute_scripts=True, min_qv=args.min_qv, use_sge=args.use_sge)
+        sv_prefix = bed2prefix(bed_record)
+        work_dir = realpath(op.join(out_dir, sv_prefix))
+        log.info("Processing sv %s" % sv_prefix)
+        print("Processing sv %s" % sv_prefix)
+        polish_a_sv(bed_record, alns, work_dir, subreads_ds_obj, reference_fasta_obj, make_reference_fa=True, make_subreads_bam=True, make_scripts=True, execute_scripts=True, min_qv=args.min_qv, use_sge=args.use_sge)
 
     reference_fasta_obj.close()
     ofile_obj.close()
